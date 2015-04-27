c56e883bacd84ab336b03b00d857c748157dd41c
v24342317 <qiaoliang2100@163.com>
2015-04-27 09:41:41 +0800

    初始化

---
+++ Assets/class1/HelloITween.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloITween : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //当前的物体5秒移动到3.3.3的坐标
+        iTween.MoveTo(this.gameObject,new Vector3(3,3,3),5f);
+	}
+}
---
+++ Assets/class1/HelloITween2.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloITween2 : MonoBehaviour {
+
+	// Use this for initialization
+    Hashtable htl = new Hashtable();
+	void Start () {
+        htl.Add("x",3);
+        htl.Add("time",2);
+        //延时：停留多久
+        htl.Add("delay",1);
+        //循环模式
+        htl.Add("looptype",iTween.LoopType.pingPong);
+
+        iTween.MoveTo(this.gameObject,htl);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
