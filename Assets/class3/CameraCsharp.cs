b01d4ed81a4e99aaa4f30e0fcc96d95a26f876a0
v24342317 <qiaoliang2100@163.com>
2015-05-13 17:43:11 +0800

    class3 add

---
+++ Assets/class3/CameraCsharp.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraCsharp : MonoBehaviour {
+
+    public GameObject planObj;
+    public GameObject ball;
+
+    private Vector3[] paths=new Vector3[3];
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit)) {
+            if (hit.transform.tag == "flat")
+            {
+                iTween.MoveUpdate(planObj,new Vector3(hit.point.x,planObj.transform.position.y,hit.point.z),.1f);
+                float dist=Vector3.Distance(gameObject.transform.position,planObj.transform.position);
+
+                float x = 0.3f - dist * 0.01f;
+                float z = 0.3f - dist * 0.01f;
+                planObj.transform.localScale = new Vector3(x,planObj.transform.localScale.y,z);
+
+                             
+            }
+        }
+        
+	    if(Input.GetMouseButtonUp(0)){
+                paths[0] = new Vector3(0,0,0);
+                paths[1] = new Vector3(planObj.transform.position.x / 2, 4f, planObj.transform.position.z/2);
+                paths[2] = new Vector3(planObj.transform.position.x, 0.5f, planObj.transform.position.z);
+                GameObject newBall=(GameObject)Instantiate(ball, new Vector3(0,0,0), Quaternion.identity);
+                iTween.MoveTo(newBall, iTween.Hash("path",paths,"movetopath",true,"orienttopath",false, "time", 1f, "easetype", iTween.EaseType.linear));
+                Destroy(newBall, 2f);
+        }
+	}
+
+    void OnDrawGizmos()
+    {
+        iTween.DrawLine(paths, Color.red);
+        iTween.DrawPath(paths, Color.blue);
+    }
+
+}
---
+++ Assets/class3/flatCsharp.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flatCsharp : MonoBehaviour {
+
+    private Color c;
+    private Texture origPic;
+    private int index;
+
+    private Vector3 screenPoint;
+    private Vector3 scanPos;
+    private Vector3 offset;
+    private Vector3 origPos;
+
+    static public bool bDrag;
+    // Use this for initialization
+    void Start()
+    {
+        c = renderer.material.color;
+        origPic = renderer.material.mainTexture;
+        //pic = new Texture2D[4];
+        index = 0;
+        scanPos = this.transform.position;
+        origPos = scanPos;
+        bDrag = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnMouseDown()
+    {
+        print("Mouse down");
+        renderer.material.color = c;
+     
+        index++;
+        if (index == 3)
+            index = 0;
+
+        /////
+        screenPoint = Camera.main.WorldToScreenPoint(scanPos);
+        offset = scanPos - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        //print(curPosition);
+        transform.position = curPosition;
+    }
+    void OnMouseEnter()
+    {
+        print("Mouse enter");
+        index = 0;
+        renderer.material.color = Color.red;
+    }
+    void OnMouseExit()
+    {
+        print("Mouse exit");
+        renderer.material.color = c;
+        renderer.material.mainTexture = origPic;
+    }
+
+    void OnMouseDrag()
+    {
+        bDrag = true;
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+        print("curPosition" + curPosition);
+    }
+
+    void OnMouseUp()
+    {
+        print("mouse up");
+        //transform.TransformPoint(origPos);
+        bDrag = false;
+        transform.position = origPos;
+    }
+
+}
